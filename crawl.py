28a4fd94c47755067a43715bf2628c7a5d546970
Taemin Lee <persuade@gmail.com>
2017-08-11 18:05:41 +0900

    let's run

--- crawl.py
+++ crawl.py
@@ -10,10 +10,12 @@ import dbutil
 from contextlib import suppress
 from hashlib import md5
 from steem.blockchain import Blockchain
+from steem.utils import block_num_from_hash
 
 class Article:
     subject = ""
     content = ""
+    text = ""
     author = ""
     preview = ""
     url = ""
@@ -24,31 +26,32 @@ class Article:
     
 
 def run():
-    conn = dbutil.getConnection()
+    conn = dbconn.getConnection()
+    lastId = dbutil.get_last_blockId(conn)
     b = Blockchain()
     for block in b.stream_from(full_blocks=True):
+        dbutil.insert_blockId(conn, block_num_from_hash(block['block_id']), block['timestamp'])
         print(block['block_id'])
         btxs = list(map(lambda x:x['operations'][0], block['transactions']))
         for tx in btxs:
             if tx[0] == 'comment' and tx[1]['parent_author'] == '':
                 meta = json.loads(tx[1]['json_metadata'])
                 print(meta['tags'])
-                #if "kr" in meta['tags']:
-                article = Article()
-                article.author = tx[1]['author']
-                article.subject = tx[1]['title']
-                article.content = tx[1]['body']
-                article.tag = meta['tags']
-                html = mistune.markdown(article.content)
-                text = html2text.html2text(html)
-                article.preview = text[0:100]
-                article.hashId = base64.b64encode(md5(article.preview.encode("UTF-8")).digest())[0:32]
-                article.created = datetime.datetime.strptime(block['timestamp'], '%Y-%m-%dT%H:%M:%S')
-                article.url = "https://steemkr.com/@" + article.author + "/" + tx[1]['permlink']
-                if "image" in meta:
-                    article.img = meta['image']
-                if article.content.startswith("@@ ", ) == False:
-                    print("upload to db")
+                if "kr" in meta['tags'] and tx[1]['body'].startswith("@@ ", ) == False:
+                    article = Article()
+                    article.author = tx[1]['author']
+                    article.subject = tx[1]['title']
+                    article.content = tx[1]['body']
+                    article.tag = meta['tags']
+                    html = mistune.markdown(article.content)
+                    article.text = html2text.html2text(html)
+                    article.preview = article.text[0:100]
+                    article.hashId = base64.b64encode(md5(article.preview.encode("UTF-8")).digest())[0:32]
+                    article.created = datetime.datetime.strptime(block['timestamp'], '%Y-%m-%dT%H:%M:%S')
+                    article.url = "https://steemkr.com/@" + article.author + "/" + tx[1]['permlink']
+                    if "image" in meta:
+                        article.img = meta['image']
+                    dbutil.insert_article(conn, article)
     
 if __name__ == '__main__':
     run()
--- dbutil.py
+++ dbutil.py
@@ -2,13 +2,26 @@ import pymysql.cursors
 
 def get_last_blockId(conn):
     with conn.cursor() as cursor:
-        sql = "SELECT id from maaum_news_st_block ORDER BY timestamp desc limit 1"
+        sql = "SELECT id from maaum_news_st_block ORDER BY id desc limit 1"
         cursor.execute(sql)
         result = cursor.fetchone()
+        if result is None:
+            return 14267112 # 14267112 / 2017.08.4
         return result['id']
 
 def insert_blockId(conn, id, timestamp):
     with conn.cursor() as cursor:
         sql = "INSERT INTO maaum_news_st_block (id, timestamp) VALUES (%s, %s)"
         cursor.execute(sql, (id, str(timestamp)))
+    conn.commit()
+
+def insert_article(conn, article):
+    nno = 0
+    with conn.cursor() as cursor:
+        sql = "INSERT INTO `maaum_news_st`(`url`, `title`, `press`, `gcat`, `gcid`, `img`, `description`, `created_at`, `update_date`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        cursor.execute(sql, (article.url, article.subject, article.author, article.hashId, article.hashId, article.img, article.preview, article.created, article.created))
+        nno = cursor.lastrowid
+    with conn.cursor() as cursor:
+        sql = "INSERT INTO `maaum_news_st_text`(`nno`, `md`, `text`) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (nno, article.content, article.text))
     conn.commit()
